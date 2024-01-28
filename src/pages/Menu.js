9770e7c87a64e66399e240c0c0ab512850ff714d
ayadav16 <ayadav7@binghamton.edu>
2024-01-28 21:43:35 +0530

    added home page

---
+++ src/pages/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import menu from '../data/menu.json';
+import { Link } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+const MenuStyles = styled(motion.div)`
+    .items{
+
+    }
+`;
+const Menu = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default Menu;
